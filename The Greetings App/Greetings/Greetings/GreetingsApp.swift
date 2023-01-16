8aac8ac359c4019f33c2d06008f369143ac75fa9
xiyuanzhou <xiyuan.zhou@sjsu.edu>
2023-01-15 19:31:43 -0800

    first learned app, updated readme

---
+++ The Greetings App/Greetings/Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Lucas on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
